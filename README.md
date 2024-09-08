71842c18c9da44de41c3728844d26819f627ab1a
DavidRR-F <david.rosefranklin96@gmail.com>
2024-09-08 12:52:45 -0400

    init

---
+++ README.md
@@ -0,0 +1,77 @@
+# Quick Domains
+
+A fast wezterm ssh domain finder to quickly attach to your defined ssh sessions
+
+## Quick Look
+
+### Dependencies
+
+There are no package dependencies but this plugin does assume you have configured a 
+`.ssh/config` as defined in the [WezTerm](https://wezfurlong.org/wezterm/config/lua/wezterm/enumerate_ssh_hosts.html) documentation
+
+```bash
+Host aur.archlinux.org
+  IdentityFile ~/.ssh/aur
+  User aur
+
+Host 192.168.1.*
+  ForwardAgent yes
+  ForwardX11 yes
+
+Host woot
+  User someone
+  Hostname localhost
+```
+
+### Setup
+
+#### Install
+
+This is a wezterm plugin. It can be installed by importing the repo and calling the apply_to_config-function. It is important that the apply_to_config-function is called after keys and key_tables have been set.
+```lua 
+local domains = wezterm.plugin.require("https://github.com/DavidRR-F/quick_domains.wezterm")
+domains.apply_to_config(config)
+```
+
+#### Configure
+
+The apply_to_config-function takes a second param opts. To override any options simply pass a table of the desired changes.
+
+```lua
+domains.apply_to_config(
+  config,
+  {
+    keys = {
+      attach = {
+        key  = 'd',
+        mods = 'CTRL',
+        tbl  = ''
+      }
+    }
+  }
+)
+```
+
+##### Defaults
+
+```lua 
+{
+  keys = {
+    attach = {
+      -- mod keys for fuzzy domain finder
+      mods = 'CTRL',
+      -- base key for fuzzy domain finder
+      key = 'd',
+      -- key table to insert key map to if any
+      tbl = '',
+    }
+  },
+  icons = {
+    -- title icon
+    hosts = '',
+    -- ssh domain icons
+    ssh = '󰣀',
+  }
+}
+
+```
---
+++ plugin/init.lua
@@ -0,0 +1,86 @@
+local wez = require "wezterm"
+local act = wez.action
+
+local pub = {}
+
+local default_settings = {
+  keys = {
+    attach = {
+      mods = 'CTRL',
+      key = 'd',
+      tbl = '',
+    }
+  },
+  icons = {
+    hosts = '',
+    ssh = '󰣀',
+  }
+}
+
+local function get_ssh_domains(opts)
+  local ssh = {}
+
+  for host, _ in pairs(wez.enumerate_ssh_hosts()) do
+    table.insert(ssh, {
+      label = opts.icons.ssh .. ' ' .. host,
+      id = host,
+    })
+  end
+
+  return ssh
+end
+
+local function get_choices(opts)
+  -- for later to concat other domain tables
+  local sessions = get_ssh_domains(opts)
+  return sessions
+end
+
+local function fuzzy_attach_to_domain(opts)
+  return wez.action_callback(function(window, pane)
+    local choices = get_choices(opts)
+
+    window:perform_action(
+      act.InputSelector({
+        action = wez.action_callback(function(window, pane, id, label)
+          if id then
+            window:perform_action(
+              act.AttachDomain(id),
+              pane
+            )
+          end
+        end),
+        title = "Choose SSH Host",
+        description = "Select a host and press Enter = accept, Esc = cancel, / = filter",
+        fuzzy_description = opts.icons.hosts .. " " .. "Hosts: ",
+        choices = choices,
+        fuzzy = true,
+      }),
+      pane
+    )
+  end)
+end
+
+function pub.apply_to_config(config, user_settings)
+  local opts = setmetatable(user_settings or {}, { __index = default_settings })
+  local keys = {
+    {
+      key = opts.keys.attach.key,
+      mods = opts.keys.attach.mods,
+      tbl = opts.keys.attach.tbl,
+      action = fuzzy_attach_to_domain(opts)
+    },
+  }
+  for _, key in ipairs(keys) do
+    if key.tbl ~= '' then
+      config.key_tables = config.key_tables or {}
+      config.key_tables[key.tbl] = config.key_tables[key.tbl] or {}
+      table.insert(config.key_tables[key.tbl], { key = key.key, mods = key.mods, action = key.action })
+    else
+      config.keys = config.keys or {}
+      table.insert(config.keys, { key = key.key, mods = key.mods, action = key.action })
+    end
+  end
+end
+
+return pub
